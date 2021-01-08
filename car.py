a45c2cac66bc3078dfc07fba7492f4063d11ef62
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2021-01-08 12:00:16 -0600

    add inheritance

--- car.py
+++ car.py
@@ -40,27 +40,3 @@ car1.drive()  # equal to 80
 car1.check_gas()
 car1.fill_tank()
 car1.check_gas()
-
-
-class Cat():
-    def __init__(self, color, nice, fav_toy):
-        self.color = color
-        self.nice = nice
-        self.fav_toy = fav_toy
-        self.age = 1
-
-    def __str__(self):
-        return '{}, {}, {}'.format(self.color, self.nice, self.fav_toy)
-
-    def birthday(self):
-        self.age + 1
-        print('Stella is now', self.age + 1)
-        print('Happy birthday!')
-
-
-stella = Cat('black and white', False, 'Chick-fl-a straw')
-piper = Cat('white', True, 'hairtie')
-
-print(stella)
-print(piper)
-stella.birthday()
